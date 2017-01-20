e54fe91c3e22bfac374d37c6bdafb5082f74138a
zamgi <denisov72@mail.ru>
2017-01-20 14:12:02 +0300

    Update --clear-after-build--.cmd

--- --clear-after-build--.cmd
+++ --clear-after-build--.cmd
@@ -1,5 +1,5 @@
-del "*.suo" /Q 2> nul
-del "url.web.demo\*.pdb" /Q 2> nul
-rd "url.web.demo\obj" /S/Q 2> nul
-rd "UrlDetector\bin" /S/Q 2> nul
-rd "UrlDetector\obj" /S/Q 2> nul
+del "*.suo" /Q
+del "url.web.demo\*.pdb" /Q
+rd "url.web.demo\obj" /S/Q
+rd "UrlDetector\bin" /S/Q
+rd "UrlDetector\obj" /S/Q
