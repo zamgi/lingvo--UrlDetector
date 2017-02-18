e7fd30564ce694a7c3dd4c6d02041d82f47a9b95
zamgi <denisov72@mail.ru>
2017-02-18 03:06:17 +0300

    Update --clear-after-build--.cmd

--- --clear-after-build--.cmd
+++ --clear-after-build--.cmd
@@ -1,6 +1,6 @@
-del "*.suo" /Q/A
+del "*.suo" /S/Q/F/A
+del "*.csproj.user" /S/Q/F/A
 del "url.web.demo\bin\*.pdb" /Q
-del "url.web.demo\*.csproj.user" /Q
 rd "url.web.demo\obj" /S/Q
 rd "UrlDetector\bin" /S/Q
 rd "UrlDetector\obj" /S/Q
